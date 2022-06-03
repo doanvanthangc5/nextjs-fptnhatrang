bb829495781095ca8aab1777e75e12d9f1be0b45
doanvanthangc5 <gianghungc5@gmail.com>
2022-06-03 09:08:21 +0700

    Add files via upload

---
+++ babyv2/js/main.js
@@ -0,0 +1,323 @@
+var getUrl = location.origin,
+    baseUrl = getUrl + "/";
+
+function getHeight() {
+    var o = $(window).height() - 71;
+    $("#main-slider .slide-banner").css("height", o)
+}
+
+function getHeight1() {
+    var o = $(window).height() - 71;
+    $(".banner-agency .slide-banner").css("height", o)
+}
+
+function copyGiftCode() {
+    try {
+        var o = "" !== document.getElementById("gifcode_vng").value ? "successful" : "unsuccessful";
+        console.log("Copying text command was " + o);
+        //var t = confirm("Chúc mừng Quý khách đã nhận được mã khuyến mãi KM93799ZL3 \n Nhấn OK để tiếp tục đăng ký.");
+       // if (o === true) {
+            var e = $("#gifcode_vng").val();
+            document.location.href = "/dang-ky?giftcode=" + e
+       // }
+    } catch (o) {
+        console.log("Oops, unable to copy")
+    }
+}
+
+function copyGiftCodeSuper80() {
+    try {
+        var o = "" !== document.getElementById("gifcode_Super80").value ? "successful" : "unsuccessful";
+        console.log("Copying text command was " + o);
+       // var t = confirm("Chúc mừng Quý khách đã nhận được mã khuyến mãi KM9379F6NT \n Nhấn OK để tiếp tục đăng ký.");
+       // if (o === true) {
+            var e = $("#gifcode_Super80").val();
+            document.location.href = "/dang-ky?giftcode=" + e
+       // }
+    } catch (o) {
+        console.log("Oops, unable to copy")
+    }
+}
+
+function copyGiftCodeSuper100() {
+    try {
+        var o = "" !== document.getElementById("gifcode_Super100").value ? "successful" : "unsuccessful";
+        console.log("Copying text command was " + o);
+        //var t = confirm("Chúc mừng Quý khách đã nhận được mã khuyến mãi KM9379F5NT \n Nhấn OK để tiếp tục đăng ký.");
+       // if (o === true) {
+            var e = $("#gifcode_Super100").val();
+            document.location.href = "/dang-ky?giftcode=" + e
+       // }
+    } catch (o) {
+        console.log("Oops, unable to copy")
+    }
+}
+
+function copyGiftCodeSuper150() {
+    try {
+        var o = "" !== document.getElementById("gifcode_Super150").value ? "successful" : "unsuccessful";
+        console.log("Copying text command was " + o);
+        //var t = confirm("Chúc mừng Quý khách đã nhận được mã khuyến mãi KM9379F4NT \n Nhấn OK để tiếp tục đăng ký.");
+        //if (o === true) {
+            var e = $("#gifcode_Super150").val();
+            document.location.href = "/dang-ky?giftcode=" + e
+       // }
+    } catch (o) {
+        console.log("Oops, unable to copy")
+    }
+}
+
+function copyGiftCodeSuper200() {
+    try {
+        var o = "" !== document.getElementById("gifcode_Super200").value ? "successful" : "unsuccessful";
+        console.log("Copying text command was " + o);
+       // var t = confirm("Chúc mừng Quý khách đã nhận được mã khuyến mãi KM9379F3NT \n Nhấn OK để tiếp tục đăng ký.");
+       // if (o === true) {
+            var e = $("#gifcode_Super200").val();
+            document.location.href = "/dang-ky?giftcode=" + e
+        //}
+    } catch (o) {
+        console.log("Oops, unable to copy")
+    }
+}
+
+function copyGiftCodeSuper250() {
+    try {
+        var o = "" !== document.getElementById("gifcode_Super250").value ? "successful" : "unsuccessful";
+        console.log("Copying text command was " + o);
+        //var t = confirm("Chúc mừng Quý khách đã nhận được mã khuyến mãi KM9379F2NT \n Nhấn OK để tiếp tục đăng ký.");
+       // if (o === true) {
+            var e = $("#gifcode_Super250").val();
+            document.location.href = "/dang-ky?giftcode=" + e
+        //}
+    } catch (o) {
+        console.log("Oops, unable to copy")
+    }
+}
+//bussiness
+function copyGiftCodeSuper400() {
+    try {
+        var o = "" !== document.getElementById("gifcode_Super400").value ? "successful" : "unsuccessful";
+        console.log("Copying text command was " + o);
+        //var t = confirm("Chúc mừng Quý khách đã nhận được mã khuyến mãi KM9379F5NT \n Nhấn OK để tiếp tục đăng ký.");
+       // if (o === true) {
+            var e = $("#gifcode_Super400").val();
+            document.location.href = "/dang-ky?giftcode=" + e
+       // }
+    } catch (o) {
+        console.log("Oops, unable to copy")
+    }
+}
+function copyGiftCodeSuper500() {
+    try {
+        var o = "" !== document.getElementById("gifcode_Super500").value ? "successful" : "unsuccessful";
+        console.log("Copying text command was " + o);
+        //var t = confirm("Chúc mừng Quý khách đã nhận được mã khuyến mãi KM9379F5NT \n Nhấn OK để tiếp tục đăng ký.");
+       // if (o === true) {
+            var e = $("#gifcode_Super500").val();
+            document.location.href = "/dang-ky?giftcode=" + e
+       // }
+    } catch (o) {
+        console.log("Oops, unable to copy")
+    }
+}
+//combo
+function copyGiftCodeSuper150combo() {
+    try {
+        var o = "" !== document.getElementById("gifcode_Super150combo").value ? "successful" : "unsuccessful";
+        console.log("Copying text command was " + o);
+        //var t = confirm("Chúc mừng Quý khách đã nhận được mã khuyến mãi KM9379F2NT \n Nhấn OK để tiếp tục đăng ký.");
+       // if (o === true) {
+            var e = $("#gifcode_Super150combo").val();
+            document.location.href = "/dang-ky?giftcode=" + e
+        //}
+    } catch (o) {
+        console.log("Oops, unable to copy")
+    }
+}
+function copyGiftCodeSuper80combo() {
+    try {
+        var o = "" !== document.getElementById("gifcode_Super80combo").value ? "successful" : "unsuccessful";
+        console.log("Copying text command was " + o);
+        //var t = confirm("Chúc mừng Quý khách đã nhận được mã khuyến mãi KM9379F2NT \n Nhấn OK để tiếp tục đăng ký.");
+       // if (o === true) {
+            var e = $("#gifcode_Super80combo").val();
+            document.location.href = "/dang-ky?giftcode=" + e
+        //}
+    } catch (o) {
+        console.log("Oops, unable to copy")
+    }
+}
+function copyGiftCodeSuper100combo() {
+    try {
+        var o = "" !== document.getElementById("gifcode_Super100combo").value ? "successful" : "unsuccessful";
+        console.log("Copying text command was " + o);
+        //var t = confirm("Chúc mừng Quý khách đã nhận được mã khuyến mãi KM9379F2NT \n Nhấn OK để tiếp tục đăng ký.");
+       // if (o === true) {
+            var e = $("#gifcode_Super100combo").val();
+            document.location.href = "/dang-ky?giftcode=" + e
+        //}
+    } catch (o) {
+        console.log("Oops, unable to copy")
+    }
+}
+
+function getQueryVariable(o) {
+    for (var t = window.location.search.substring(1).split("&"), e = 0; e < t.length; e++) {
+        var a = t[e].split("=");
+        if (a[0] == o) return a[1]
+    }
+    return !1
+}
+
+function displayNotify() {
+    for (var o in globalStorage)
+        if ("1" === globalStorage[o].is_push && 1 === globalStorage[o].is_show) {
+            var t = globalStorage[o];
+            printTemplate(t.name, t.excerpt, t.content_url), globalStorage[o].is_push = "2"
+        }
+}
+
+function printTemplate(o, t, e) {
+    var a = "<div class='box-notf'><div class='row'><div class='col-xs-4 col-sm-4'><div class='image'>" + ("<a href='" + (e = e.replace("https://fptnhatrang.vn", baseUrl)) + "' target='_blank'><img src='/html-v2/images/logo2-01.png'/></a>") + "</div></div><div class='col-xs-8 col-sm-8'><h4 class='title'>" + ("<a href='" + e + "' target='_blank'>" + o + "</a>") + "</h4><p class='excerpt'>" + ("<a href='" + e + "' target='_blank'>" + t + "</a>") + "</p></div></div><div class='close-btn'><i class='fa fa-times'></i></div></div>";
+    $(".notf-area").append(a)
+}
+
+function closeNotf() {
+    $("html").on("click", ".close-btn", function() {
+        $(this).closest("div.box-notf").remove()
+    })
+}
+
+function submitModal() {
+    $("html").on("click", "#ObjectModal .btn-save", function() {
+        var t = $(this),
+            o = {
+                url: $("#ObjectModal form").data("action"),
+                type: "post",
+                dataType: "json",
+                beforeSend: function() {
+                    t.addClass("disabled")
+                },
+                uploadProgress: function(o, t, e, a) {},
+                success: function(o) {
+                    "error" == o.status ? $("#ObjectModal .modal-status").html(o.error_messages) : ($("#ObjectModal .modal-body").html('<div class="modal-status"></div>'), $("#ObjectModal .modal-status").html(o.success_messages), $("#ObjectModal .modal-footer").html(o.action_group))
+                },
+                complete: function(o) {
+                    t.removeClass("disabled")
+                },
+                error: function() {}
+            };
+        $("#ObjectModal form").ajaxForm(o), $("#ObjectModal form").submit()
+    })
+}
+
+function openModal() {}
+$(document).ready(function() {
+    function o() {
+        var o = $(this).find(".modal-dialog");
+        o.css("margin-top", Math.max(0, ($(window).height() - o.height()) / 2))
+    }
+    if (closeNotf(), submitModal(), $("html").on("click", ".btn-object-modal", function() {
+            var o = $(this).attr("modal-title");
+            $("#ObjectModal .modal-title").html(o), $("#ObjectModal .modal-status").html("");
+            var t = $(this).attr("modal-size");
+            $("#ObjectModal .modal-dialog").removeClass("modal-lg"), $("#ObjectModal .modal-dialog").removeClass("modal-sm"), $("#ObjectModal .modal-dialog").addClass(t), $("#ObjectModal .modal-body").html('<div class="text-center"><i class="fa fa-spin fa-spinner"></i> loading ...</div>');
+            var e = $(this).attr("modal-url");
+            return $.ajax({
+                type: "GET",
+                url: e,
+                dataType: "json",
+                cache: !1,
+                crossDomain: "true",
+                success: function(o) {
+                    $("#ObjectModal .modal-content").html(o.item_view), null != o.action_group && $("#ObjectModal .modal-footer").html(o.action_group)
+                },
+                error: function(o, t) {}
+            }), $("#ObjectModal").modal(), !1
+        }), $(".modal").on("shown.bs.modal", o), $(window).on("resize", function() {
+            $(".modal:visible").each(o)
+        }), 0 < $("#main-slider").length) {
+        var t = $("#main-slider");
+        $("#main-slider").owlCarousel({
+            autoPlay: 3e3,
+            stopOnHover: !0,
+            navigation: !0,
+            paginationSpeed: 1e3,
+            goToFirstSpeed: 2e3,
+            singleItem: !0,
+            transitionStyle: "fade"
+        });
+        var e = getQueryVariable("banner");
+        "" != e && t.trigger("owl.goTo", e)
+    }
+    $(".tool-tip, .mac-code button").click(function() {
+        $("html, body").animate({
+            scrollTop: $("#view-info").offset().top - 150
+        }, 1e3)
+    });
+    var a = getQueryVariable("tab");
+    "" != a && $('.nav-tabs a[href="#' + a + '"]').tab("show"), $(window).width() < 768 && $(".dropdown .dropdown-toggle").removeClass("disabled"), getHeight(), $(window).resize(function() {
+        getHeight()
+    }), getHeight1(), $(window).resize(function() {
+        getHeight1()
+    }), 0 < $(".tooltipstered").length && $(".tooltipstered").tooltipster({
+        contentAsHTML: !0,
+        interactive: !0,
+        trigger: "click"
+    }), 0 < $(".select2").length && $(".select2").select2({
+        theme: "bootstrap"
+    }), 0 < $(".date-start").length && $(".date-start").datetimepicker({
+        format: "DD-MM-YYYY"
+    }), 0 < $(".date-end").length && $(".date-end").datetimepicker({
+        format: "DD-MM-YYYY"
+    }), $('[data-toggle="tooltip"]').tooltip({
+        html: !0,
+        container: "body"
+    }), $("#chat_fpt_play").click(function(o) {
+        o.preventDefault(), $("#facebook").stop().animate({
+            height: "toggle"
+        })
+    }), $("body").scrollspy({
+        target: "#scrollspy",
+        offset: 90
+    }), $("#scrollspy a").on("click", function(o) {
+        o.preventDefault();
+        var t = this.hash;
+        $("html, body").animate({
+            scrollTop: $(this.hash).offset().top - 90
+        }, 1e3, function() {
+            window.location.hash = t
+        })
+    }), $("#modal_banner").modal(), $(window).on("load resize", function() {
+        var o = $(window).width();
+        992 < o ? $(".carousel").carousel({
+            directionNav: !1,
+            buttonNav: "none",
+            carouselWidth: 940,
+            carouselHeight: 390,
+            autoplayInterval: 6e3,
+            buttonNav: "bullets",
+            frontWidth: 610,
+            frontHeight: 343
+        }) : 748 < o ? $(".carousel").carousel({
+            carouselWidth: 720,
+            carouselHeight: 270,
+            autoplayInterval: 6e3,
+            buttonNav: "bullets",
+            frontWidth: 400,
+            frontHeight: 225
+        }) : $(".carousel").carousel({
+            carouselWidth: 300,
+            carouselHeight: 150,
+            autoplayInterval: 6e3,
+            buttonNav: "bullets",
+            frontWidth: 200,
+            frontHeight: 113
+        })
+    })
+}), $("#navbar ul li>a").hover(function() {
+    $(this).addClass("hover")
+}, function() {
+    $(this).removeClass("hover")
+});
